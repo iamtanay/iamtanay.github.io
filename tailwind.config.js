976fcfd8c047232f0517615ae8ab833f99aff7b3
Tanay Kashyap <iamtanay@hotmail.com>
2025-06-20 07:30:13 +0530

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -61,6 +61,9 @@ module.exports = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      backdropBlur: {
+        xs: '2px',
       }
     },
   },
